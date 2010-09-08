8a75acfc85bfa01213c7c4b66a6f313e53a8afec
Joachim Van der Auwera <joachim@progs.be>
2010-09-08 06:43:52 +0000

    GWT-107

---
+++ geomajas-gwt-archetype/src/main/resources/archetype-resources/src/main/java/client/pages/AbstractTab.java
@@ -0,0 +1,36 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client.pages;
+
+import org.geomajas.gwt.client.widget.MapWidget;
+
+import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.tab.Tab;
+
+/**
+ * Basic implementation for a tab. The tabs are added bottom left.
+ * 
+ * @author Jan De Moerloose
+ */
+public abstract class AbstractTab extends Tab {
+
+	protected final MapWidget map;
+
+	protected VLayout mainLayout;
+
+	AbstractTab(String title, MapWidget map) {
+		super(title);
+		this.map = map;
+		mainLayout = new VLayout();
+		mainLayout.setWidth100();
+		mainLayout.setHeight100();
+		setPane(mainLayout);
+	}
+
+	public abstract void initialize();
+
+	public MapWidget getMap() {
+		return map;
+	}
+}
